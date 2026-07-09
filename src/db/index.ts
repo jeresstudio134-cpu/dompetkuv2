3c0d367a403c1f583c155e34a4af2ba714a7f101
jeresstudio134-cpu <jeresstudio134@gmail.com>
2026-07-09 10:52:48 +0700

    Update index.ts

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,6 +1,6 @@
 import { drizzle } from "drizzle-orm/neon-http";
 import { neon } from "@neondatabase/serverless";
-import * as schema from "./schema";
+import * as schema from "./schema.js";
 
 let db: ReturnType<typeof drizzle> | null = null;
 
